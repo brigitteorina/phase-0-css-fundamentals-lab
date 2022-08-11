9e40cc54735efa4750098d077e7012e236e01920
Brigitte Orina <brigitteorina@gmail.com>
2022-08-11 16:30:46 +0300

    Compleated assignment

---
+++ incubator.php
@@ -0,0 +1,30 @@
+<h3>Contact</h3>
+
+<div>
+  
+  <p >
+    <a href="mailto:info@exceptionalrealty.com">info@exceptionalrealty.com</a><br>
+    +1 710 555 1234<br>
+    @fakeRealty
+  </p>
+</div>
+
+<div>
+<h3>Correspondence</h3>
+
+  <p>
+    Exceptional Realty<br>
+    P.O. Box 5555<br>
+    Brooklyn, NY 11201
+  </p>
+</div>
+
+<h3>Links</h3>
+
+<div>
+  <p>
+    <a href="#" target="_blank">Home Restoration</a><br>
+    <a href="#" target="_blank">Affordable Mortgages</a><br>
+    <a href="#" target="_blank">Professional Moving Crew</a>
+  </p>
+</div>
